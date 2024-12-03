522d8a5704db63b201a138de02adfb22908b3a80
reaper-js <jatinsaini191@gmail.com>
2024-12-03 13:22:37 +0530

    Removed Cors

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
 
 app.use(express.json());
 
