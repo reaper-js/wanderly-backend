84cbcabab4acbd467bc8308576b6f36a601b42d6
reaper-js <jatinsaini191@gmail.com>
2024-12-24 19:22:23 +0530

    Added Rate Limiters

--- src/routes/General.js
+++ src/routes/General.js
@@ -1,16 +1,17 @@
 import { attractions, autocompleteSearch, getPhoto, getRoute, geocodeAddress, getMapData, placeDetails, fetchWeather} from "../controllers/General.js";
 import express from 'express';
+import { apiLimiter } from '../middleware/rateLimiter.js';
 const router = express.Router();
 
 //auto complete search destinations
-router.get('/autocompleteSearch', autocompleteSearch);
-router.get('/getPlaceDetails', placeDetails);
-router.get('/photos/:photoReference', getPhoto); // New route for photo proxy
-router.get('/attractions', attractions);
-router.get('/fetchWeather/:city', fetchWeather);
-router.post('/getRoute', getRoute);
-router.get('/getMapData', getMapData);
-router.post('/geocodeAddress', geocodeAddress);
+router.get('/autocompleteSearch', apiLimiter, autocompleteSearch);
+router.get('/getPlaceDetails', apiLimiter, placeDetails);
+router.get('/photos/:photoReference', apiLimiter, getPhoto); // New route for photo proxy
+router.get('/attractions', apiLimiter, attractions);
+router.get('/fetchWeather/:city', apiLimiter, fetchWeather);
+router.post('/getRoute', apiLimiter, getRoute);
+router.get('/getMapData', apiLimiter, getMapData);
+router.post('/geocodeAddress', apiLimiter, geocodeAddress);
 // router.get('/getPlacePhotos', getPlacePhotos);
 
 export default router;
--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import {login, signup, logoutAll, logoutOne, saveTrip, startTrip, editTrip, endTrip, getTrips, getTrip, updateProfile, startExistingTrip, getUserStatus, deleteTrip} from '../controllers/User.js'
 import auth from '../middleware/auth.js'
+import { authLimiter, generalLimiter } from '../middleware/rateLimiter.js'
 
 const router = express.Router();
 
 
-router.post('/login', login);
-router.post('/signup', signup);
-router.post('/logout', auth, logoutOne);
-router.post('/logoutAll', auth, logoutAll);
+router.post('/login', authLimiter, login);
+router.post('/signup', authLimiter, signup);
+router.post('/logout', generalLimiter, auth, logoutOne);
+router.post('/logoutAll', generalLimiter, auth, logoutAll);
+router.patch('/updateProfile', generalLimiter, auth, updateProfile);
 
-router.get('/userGetTripStatus', auth, getUserStatus)
+router.get('/userGetTripStatus', generalLimiter, auth, getUserStatus)
 
-///Trip Routes
-router.post('/saveTrip', auth, saveTrip);
-router.post('/startTrip', auth, startTrip);
-router.get('/getTrips', auth, getTrips);
-router.get('/getTrip/:tripId', auth, getTrip);
-router.post('/startExistingTrip/:tripId', auth, startExistingTrip);
-router.patch('/editTrip/:tripId', auth, editTrip);
-router.post('/endTrip/:tripId', auth, endTrip);
-router.delete('/deleteTrip/:tripId', auth, deleteTrip);
+router.post('/saveTrip', generalLimiter, auth, saveTrip);
+router.post('/startTrip', generalLimiter, auth, startTrip);
+router.get('/getTrips', generalLimiter, auth, getTrips);
+router.get('/getTrip/:tripId', generalLimiter, auth, getTrip);
+router.post('/startExistingTrip/:tripId', generalLimiter, auth, startExistingTrip);
+router.patch('/editTrip/:tripId', generalLimiter, auth, editTrip);
+router.post('/endTrip/:tripId', generalLimiter, auth, endTrip);
+router.delete('/deleteTrip/:tripId', generalLimiter, auth, deleteTrip);
 
 
-router.patch('/updateProfile', auth, updateProfile);
+
 export default router;
