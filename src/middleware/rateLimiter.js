a7a392c6424e48f8175eb278f92ce2a375191fe2
reaper-js <jatinsaini191@gmail.com>
2024-12-25 20:35:58 +0530

    Changed the limits

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,19 +2,19 @@ import rateLimit from 'express-rate-limit'
 
 export const generalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 min
-    max: 100, 
-    message: 'Too many requests from this IP, please try again later'
+    max: 1000, 
+    message: 'tmrftip',
 })
 
 
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,
+    max: 20,
     message: 'Too many authentication attempts, please try again later'
 })
 
 export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
-    max: 100, 
+    max: 1000, 
     message: 'Too many API requests from this IP, please try again later'
 })
