53f09c6681ef11f26a70e000f79fc21f0df56433
Anmol Bansal <anmol13200159@gmail.com>
2025-05-19 14:41:02 +0530

    removed rate-limiters temporarily

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,20 +1,20 @@
-import rateLimit from 'express-rate-limit'
+// import rateLimit from 'express-rate-limit'
 
-export const generalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 min
-    max: 1000, 
-    message: 'tmrftip',
-})
+// export const generalLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 min
+//     max: 1000, 
+//     message: 'tmrftip',
+// })
 
 
-export const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 20,
-    message: 'Too many authentication attempts, please try again later'
-})
+// export const authLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000,
+//     max: 20,
+//     message: 'Too many authentication attempts, please try again later'
+// })
 
-export const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, 
-    max: 1000, 
-    message: 'Too many API requests from this IP, please try again later'
-})
+// export const apiLimiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, 
+//     max: 1000, 
+//     message: 'Too many API requests from this IP, please try again later'
+// })
